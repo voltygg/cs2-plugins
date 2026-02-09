08e0cc41c4f6875758b5bab8887c12d6d0f501dd
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-02-09 05:48:17 -0500

    removed redunant configs

--- env.example.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export HL2SDKCS2="C:/dev/alliedmodders/hl2sdk-cs2"
-export HL2SDKMANIFESTS="C:/dev/alliedmodders/hl2sdk-manifests"
-export MMSOURCE20="C:/dev/alliedmodders/mmsource-2.0"
