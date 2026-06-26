4ae524199b6602b3fccaf74122aa1322c6407ff3
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-06-26 18:22:13 -0400

    fix: generate only the required protos in the Linux build
    The wildcard pulled in base_gcmessages.proto -> steammessages.proto,
    which isn't on the proto path. Mirror cs2-kit/Protobuf.ambuild and
    compile only the protos the plugin actually needs.

--- deploy/scripts/build-plugin.sh
+++ deploy/scripts/build-plugin.sh
@@ -11,18 +11,24 @@ cd "$RepoRoot"
 ProtoRoot="vendor/cs2-kit/vendor/hl2sdk-cs2"
 Protoc="$ProtoRoot/devtools/bin/linux/protoc"
 
+# Only the protos cs2-kit/Protobuf.ambuild declares as required; the full
+# wildcard pulls in base_gcmessages.proto -> steammessages.proto (unneeded).
 "$Protoc" \
     --proto_path="$ProtoRoot/common" \
     --proto_path="$ProtoRoot/thirdparty/protobuf-3.21.8/src" \
     --cpp_out="$ProtoRoot/public" \
-    "$ProtoRoot"/common/*.proto
+    "$ProtoRoot/common/network_connection.proto" \
+    "$ProtoRoot/common/networkbasetypes.proto" \
+    "$ProtoRoot/common/engine_gcmessages.proto"
 
 "$Protoc" \
     --proto_path="$ProtoRoot/common" \
     --proto_path="$ProtoRoot/game/shared" \
     --proto_path="$ProtoRoot/thirdparty/protobuf-3.21.8/src" \
     --cpp_out="$ProtoRoot/game/shared" \
-    "$ProtoRoot"/game/shared/*.proto
+    "$ProtoRoot/game/shared/usermessages.proto" \
+    "$ProtoRoot/game/shared/usercmd.proto" \
+    "$ProtoRoot/game/shared/gameevents.proto"
 
 mkdir -p objdir
 cd objdir
